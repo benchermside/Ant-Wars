53c9b3de9eb37e25e8a368a4f4413cb37287166b
Michael Chermside <mcherm@mcherm.com>
2023-06-05 09:40:00 -0400

    Allow for resizing in the drawing function.

--- web_ui/js/display.js
+++ web_ui/js/display.js
@@ -3,13 +3,22 @@
  */
 
 // This is the main drawing function. It always draws to the standard game-canvas. It is
-// passed the drawing context to draw on and a Grid of TerrainIds to draw.
+// passed the drawing context to draw on, a Grid of TerrainIds to draw, and a size field
+// which gives the number of pixels wide each hex should be.
 //
-// See dataStructures.js for the definition of a TerrainId and of a Grid.
+//
+// Parameters:
+//  * drawContext - this is the "context" to draw onto. It is obtained by calling
+//       getContext("2d") on a canvas element. The capabilities are described in
+//       https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+//  * grid - this is a TerrainGrid as described in dataStructures.js
+//  * size - this is a number which gives the width that each hex should be (the
+//       height can be calculated from the width). Each time the function is called
+//       this might be different if the user has zoomed.
 //
 // NOTE: The current version is a placeholder. It just draws random junk no matter what gets
 //   passed in.
-function drawBackground(drawContext, grid) {
+function drawBackground(drawContext, terrainGrid, size) {
     let drawCircleAt = function(x,y) {
         const colors = ["red", "green", "blue", "yellow", "orange", "purple", "brown"];
         const color = colors[Math.floor(Math.random() * colors.length)];
