43f9b6fd75a92dc1079263ca2b0963bba53435ec
Michael Chermside <mcherm@mcherm.com>
2023-06-06 19:57:52 -0400

    Create an "End Turn" button and code that calls into rules.js and just makes random moves.

--- web_ui/js/main.js
+++ web_ui/js/main.js
@@ -4,10 +4,14 @@
 
 /* ========= Global Variables ========= */
 let hexSize = 20;
-let terrainGrid = [];
+let gameState = {
+    terrainGrid: [],
+    ants: [],
+};
 
 
 /* ========= Functions ========= */
+
 /*
  * Call this to re-draw all the game graphics.
  */
@@ -15,27 +19,37 @@ const render = function() {
     const gameCanvasElem = document.getElementById("game-canvas");
     const drawContext = gameCanvasElem.getContext("2d");
     drawContext.clearRect(0, 0, gameCanvasElem.width, gameCanvasElem.height);
-    drawBackground(drawContext, terrainGrid, hexSize);
+    drawBackground(drawContext, gameState.terrainGrid, hexSize);
+}
+
+/*
+ * Call this to make the turn end and render the new update.
+ */
+const endTurn = function() {
+    // === For each ant, select a random allowed destination to move to ===
+    const actionSelection = [];
+    for(let antNumber = 0; antNumber < gameState.ants.length; antNumber++) {
+        const moveLocations = possibleMoves(gameState, antNumber);
+        const randomMove = moveLocations[Math.floor(Math.random() * moveLocations.length)];
+        const action = {
+            name: "Move",
+            destination: randomMove
+        };
+        actionSelection.push(action);
+    }
+
+    // === Perform the moves ===
+    gameState = applyRules(gameState, actionSelection);
+
+    // === Render the screen ===
+    render();
 }
 
 
-/* ========= Run Stuff On Load ========= */
-
-window.onload = function() {
-    const zoomInBtnElem = document.getElementById("zoom-in-btn");
-    zoomInBtnElem.onclick = function() {
-        hexSize = hexSize * 1.2;
-        console.log("Zoom", hexSize);
-        render();
-    };
-    const zoomOutBtnElem = document.getElementById("zoom-out-btn");
-    zoomOutBtnElem.onclick = function() {
-        hexSize = hexSize / 1.2;
-        render();
-    };
-
-    const gameContentElem = document.getElementById("game-content");
-    const gameCanvasElem = document.getElementById("game-canvas");
+/*
+ * This is called before we begin to set up the initial game position.
+ */
+const initializeStartingPosition = function() {
     const startingTerrainGrid = [
         [3, 3, 3, 3, 3, 3],
         [2, 1, 1, 1, 2, 1],
@@ -43,7 +57,35 @@ window.onload = function() {
         [2, 2, 1, 1, 1, 2],
         [0, 0, 0, 0, 0, 0],
     ];
-    terrainGrid = startingTerrainGrid;
+    gameState.terrainGrid = startingTerrainGrid;
+    const startingAnts = [
+        {"location": [4, 3]}
+    ];
+    gameState.ants = startingAnts;
+}
+
+
+/* ========= Run Stuff On Load ========= */
+
+window.onload = function() {
+    // ==== Set up button actions ====
+    const zoomInBtnElem = document.getElementById("zoom-in-btn");
+    zoomInBtnElem.onclick = function() {
+        hexSize = hexSize * 1.2;
+        render();
+    };
+    const zoomOutBtnElem = document.getElementById("zoom-out-btn");
+    zoomOutBtnElem.onclick = function() {
+        hexSize = hexSize / 1.2;
+        render();
+    };
+    const endTurnBtnElem = document.getElementById("end-turn-btn");
+    endTurnBtnElem.onclick = endTurn;
+
+    // ==== Prepare Game Start ====
+    initializeStartingPosition();
+    const gameContentElem = document.getElementById("game-content");
+    const gameCanvasElem = document.getElementById("game-canvas");
     const contentBorderWidth = parseInt((getComputedStyle(gameContentElem)["borderWidth"]));
     gameCanvasElem.width = gameContentElem.offsetWidth - 2 * contentBorderWidth;
     gameCanvasElem.height = gameContentElem.offsetHeight - 2 * contentBorderWidth;
--- web_ui/js/rules.js
+++ web_ui/js/rules.js
@@ -6,10 +6,14 @@
 // and a MoveSelection. It returns the new GameState.
 //
 function applyRules(gameState, actionSelection) {
+    // As a placeholder, we'll return the gameState unmodified
+    return gameState;
 }
 
 // This finds the list of allowed locations an ant can move to. It is passed a GameState (see
 // dataStructures.js) and an integer specifying which ant we want the moves of. It returns a
 // MoveLocations.
 function possibleMoves(gameState, antNumber) {
+    // As a placeholder, we'll return the same location we are already on -- and nothing else.
+    return [ gameState.ants[antNumber].location ];
 }
