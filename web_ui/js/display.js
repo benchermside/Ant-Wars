9e2c859573a416b28bf89ca9a0a0d34a58bde06f
Genevieve <genchermside@gmail.com>
2023-06-16 15:18:43 -0400

    drew grid

--- web_ui/js/display.js
+++ web_ui/js/display.js
@@ -35,33 +35,20 @@ function hexCenter(x, y, hexSize) {
 // NOTE: The current version is a placeholder. It just draws random junk no matter what gets
 //   passed in.
 function drawBackground(drawContext, terrainGrid, hexSize) {
-
-    drawContext.lineWidth = 10;
-    drawContext.beginPath();
-    drawContext.moveTo(50, 140);
-    drawContext.lineTo(150, 60);
-    drawContext.lineTo(250, 140);
-    drawContext.closePath();
-    drawContext.stroke();
-
-    let drawCircleAt = function(x,y) {
-        const colors = ["red", "green", "blue", "yellow", "orange", "purple", "brown"];
-        const color = colors[Math.floor(Math.random() * colors.length)];
-
-        const circle = new Path2D();
-        circle.arc(x, y, hexSize / 3, 0, 2 * Math.PI);
-        drawContext.fillStyle = color;
-        drawContext.fill(circle);
-    }
-
-    // const NUM_CIRCLES = 30;
-    // for (let i=0; i<NUM_CIRCLES; i++) {
-    //     drawCircleAt(Math.random() * (hexSize * 8), Math.random() * (hexSize * 6));
-    // }
-    for(let x=0;x<5; x++) {
-        for(let y = 0; y<5; y++) {
+    for(let x=0;x<terrainGrid[0].length; x++) {
+        for(let y = 0; y<terrainGrid.length; y++) {
            const cord = hexCenter(x, y, hexSize);
-           drawCircleAt(cord[0], cord[1]);
+            drawContext.lineWidth = 10;
+            drawContext.beginPath();
+            drawContext.moveTo(cord[0], cord[1]-hexSize/Math.sqrt(3));
+            drawContext.lineTo(cord[0]+hexSize/2, cord[1]-hexSize/(2*Math.sqrt(3)));
+            drawContext.lineTo(cord[0]+hexSize/2, cord[1]+hexSize/(2*Math.sqrt(3)));
+            drawContext.lineTo(cord[0], cord[1]+hexSize/(Math.sqrt(3)));
+            drawContext.lineTo(cord[0]-hexSize/2, cord[1]+hexSize/(2*Math.sqrt(3)));
+            drawContext.lineTo(cord[0]-hexSize/2, cord[1]-hexSize/(2*Math.sqrt(3)));
+            drawContext.lineTo(cord[0], cord[1]-hexSize/(Math.sqrt(3)));
+            drawContext.closePath();
+            drawContext.stroke();
         }
     }
 
