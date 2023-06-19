847cd5e125f5d8cf92ea46683098fc0230d510d4
Michael Chermside <mcherm@mcherm.com>
2023-06-19 13:01:27 -0400

    Add tests for hexClicked()

--- web_ui/js/display.js
+++ web_ui/js/display.js
@@ -60,6 +60,49 @@ function hexClicked(gameState, hexSize, pixelCoord) {
     }
 }
 
+/*
+ * This function will run some tests of hexClicked(). If it doesn't throw an exception then the
+ * tests all pass.
+ */
+function testHexClicked() {
+    function assertEqualArrays(testVal, expected) {
+        if (testVal === null && expected === null) {
+            // success
+        } else if (testVal === null || expected === null) {
+            throw Error(`assertion failed: ${testVal} != ${expected}`);
+        } else if (testVal.length !== expected.length || !testVal.every((x,i) => x === expected[i])) {
+            throw Error(`assertion failed: ${testVal} != ${expected}`);
+        } else {
+            // success
+        }
+    }
+    const gameState = {terrainGrid: [[0,0,0], [0,0,0]]};
+    const hexSize = 100;
+    const tests = [
+        [[17,9], null],
+        [[36,17], [0,0]],
+        [[62,18], [0,0]],
+        [[85,11], null],
+        [[85,11], null],
+        [[116,10], null],
+        [[137,16], [1,0]],
+        [[15,51], [0,0]],
+        [[76,64], [0,0]],
+        [[113,59], [1,0]],
+        [[176,59], [1,0]],
+        [[216,58], [2,0]],
+        [[10,108], null],
+        [[35,98], [0,0]],
+        [[66,95], [0,0]],
+        [[88,102], [0,1]],
+        [[117,106], [0,1]],
+        [[22,144], null],
+    ];
+    tests.forEach(data => {
+        assertEqualArrays( hexClicked(gameState, hexSize, data[0]), data[1]);
+    });
+}
+testHexClicked(); // FIXME: Run at launch
 
 
 // This is the main drawing function. It always draws to the standard game-canvas. It is
