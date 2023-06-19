e6224b8e3e950dfd5cd82630eaba37e20ba7659d
Michael Chermside <mcherm@mcherm.com>
2023-06-19 13:48:58 -0400

    Refactor the hexClicked() function to make it SOMEWHAT shorter and more readable.

--- web_ui/js/display.js
+++ web_ui/js/display.js
@@ -19,40 +19,31 @@ function hexCenter(x, y, hexSize) {
  * pixel-coordinates of a location. It returns an [x,y] pair giving the grid coordinates of the
  * hexagon clicked on OR return null if the click was outside of any grid hexagon. It is ALSO
  * passed the gameState, but that is ONLY so it can find the dimensions of the grid.
+ *
+ * NOTE: There are probably still some ways to simplify the formula in this code, and maybe also
+ * ways to make it more readable.
  */
 function hexClicked(gameState, hexSize, pixelCoord) {
-    // FIXME: There is certainly a way to simplify the code for this, but I wanted to save the first working version.
+    // --- grab some obvious numbers we'll need ---
     const gridXBound = gameState.terrainGrid[0].length;
     const gridYBound = gameState.terrainGrid.length;
     const pixelX = pixelCoord[0];
     const pixelY = pixelCoord[1];
     const oneThirdHeight = hexSize / (2 * Math.sqrt(3)); // this is 1/3 the full height of a hexagon
-    const thirdsY = Math.floor(pixelY / oneThirdHeight); // how many thirds down from the top this is
+    const thirdsY = Math.floor(pixelY / oneThirdHeight); // how many thirds down from the top this is (floored to integer)
+
+    // --- deal with the "zig zag" areas where the rows of hexes overlap in the same y value ---
+    const halvesX = (2 * pixelX / hexSize) % 2; // number of half-hexes from the left-side, mod 2
+    const sYRaw = (pixelY % (2 * oneThirdHeight)) / oneThirdHeight;
+    const sX = sYRaw <= 1 ? halvesX : (halvesX + 1) % 2;
+    const sY = sYRaw % 1;
     const isInZigZag = thirdsY % 3 === 0; // whether this falls in the section where the points of hexes are fitting together
-    let zigZagAdjust = [0,0];
-    let w = "";
-    if (isInZigZag) {
-        const sXRaw = 2 * (pixelX % hexSize) / hexSize;
-        const sYRaw = (pixelY % (2 * oneThirdHeight)) / oneThirdHeight;
-        const sX = sYRaw <= 1 ? sXRaw : (sXRaw + 1) % 2;
-        const sY = sYRaw % 1;
-        if (sX <= 1) {
-            if (sX + sY < 1) {
-                zigZagAdjust = [0,-1];
-            } else {
-                zigZagAdjust = [0,0];
-            }
-        } else {
-            if ((2 - sX) + sY < 1) {
-                zigZagAdjust = [0,-1];
-            } else {
-                zigZagAdjust = [0,0];
-            }
-        }
-    }
-    const gridY = Math.floor(thirdsY / 3) + zigZagAdjust[1];
+    const zigZagAdjustY = (isInZigZag && (1 - Math.abs(sX - 1) + sY < 1)) ? -1 : 0;
+
+    // --- find the grid X and Y values ---
+    const gridY = Math.floor(thirdsY / 3) + zigZagAdjustY;
     const shiftOddRows = 0.5 * (gridY % 2); // odd numbered rows are shifted 1/2 hex to the right
-    const gridX = Math.floor((pixelX / hexSize) - shiftOddRows) + zigZagAdjust[0];
+    const gridX = Math.floor((pixelX / hexSize) - shiftOddRows);
     if (gridX < 0 || gridX >= gridXBound || gridY < 0 || gridY >= gridYBound) {
         return null;
     } else {
