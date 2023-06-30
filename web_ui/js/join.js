fd02cb4f83f1aa71f22799e62c57e4471854ac21
Michael Chermside <mcherm@mcherm.com>
2023-06-29 21:53:37 -0400

    Finished building the UI for joining a game.

--- web_ui/js/join.js
+++ web_ui/js/join.js
@@ -4,6 +4,13 @@
 
 /* ========= Functions ========= */
 
+function hideElemById(id) {
+    document.getElementById(id).classList.add("hidden");
+}
+function showElemById(id) {
+    document.getElementById(id).classList.remove("hidden");
+}
+
 /*
  * Returns true if the string "username" is a valid username.
  */
@@ -15,24 +22,62 @@ function isValidUsername(username) {
     return usernameRegex.test(username);
 }
 
+/*
+ * When this is called with data about the active games it populates the table of games
+ * to join.
+ */
+function populateGameChooser(availableGameData) {
+    if (availableGameData.length === 0) {
+        hideElemById("multi-games-widget");
+        showElemById("zero-games-widget");
+    } else {
+        const tableElem = document.getElementById("multi-games-widget");
+        const oldTBodyElem = tableElem.getElementsByTagName('tbody')[0];
+        const newTBodyElem = document.createElement('tbody');
+        tableElem.replaceChild(newTBodyElem, oldTBodyElem);
+        availableGameData.forEach(gameData => {
+            const rowElem = newTBodyElem.insertRow();
+            rowElem.innerHTML = `<tr>` +
+                `<td><input type="radio" name="game" value="${gameData.gameCode}" class="game-select"></td>` +
+                `<td>${gameData.hostUsername}</td>` +
+                `<td>${gameData.map}</td>` +
+                `<td>${gameData.rules}</td>` +
+            `</tr>`;
+            rowElem.getElementsByClassName("game-select")[0].addEventListener("change", e => {
+                document.getElementById("game-code-field").value = gameData.gameCode;
+                document.getElementById("join-game-btn").disabled = false;
+            });
+        });
+        showElemById("multi-games-widget");
+        hideElemById("zero-games-widget");
+    }
+}
+
+/*
+ * This is called when we need to refresh the data in the find-game-panel.
+ */
+function refreshFindGamePanel() {
+    const availableGameData = [
+        { gameCode: "87433", hostUsername: "mcherm", map: "Default Map", rules: "Default Rules", },
+        { gameCode: "53902", hostUsername: "grapefruit", map: "Default Map", rules: "Default Rules", },
+        { gameCode: "66839", hostUsername: "melissa", map: "Other Map", rules: "Default Rules", },
+    ];
+    populateGameChooser(availableGameData);
+}
+
+
 /* ========= Run Stuff On Load ========= */
 
 window.addEventListener("load", function() {
     // ==== Set up button actions ====
-    function hide(id) {
-        document.getElementById(id).classList.add("hidden");
-    }
-    function show(id) {
-        document.getElementById(id).classList.remove("hidden");
-    }
     const buttonActions = {
         "single-player-btn": () => {window.location.href = "/index.html";},
-        "multi-player-btn": () => {hide("pick-player-count-panel"); show("host-or-join-panel");},
-        "host-or-join-back-btn": () => {hide("host-or-join-panel"); show("pick-player-count-panel");},
-        "host-game-btn": () => {hide("host-or-join-panel"); show("host-game-panel");},
-        "host-game-back-btn": () => {hide("host-game-panel"); show("host-or-join-panel");},
-        "join-game-btn": () => {hide("host-or-join-panel"); show("join-game-panel");},
-        "join-game-back-btn": () => {hide("join-game-panel"); show("host-or-join-panel");},
+        "multi-player-btn": () => {hideElemById("pick-player-count-panel"); showElemById("host-or-join-panel");},
+        "host-or-join-back-btn": () => {hideElemById("host-or-join-panel"); showElemById("pick-player-count-panel");},
+        "host-game-btn": () => {hideElemById("host-or-join-panel"); showElemById("host-game-panel");},
+        "host-game-back-btn": () => {hideElemById("host-game-panel"); showElemById("host-or-join-panel");},
+        "find-game-btn": () => {hideElemById("host-or-join-panel"); showElemById("find-game-panel"); refreshFindGamePanel(); },
+        "find-game-back-btn": () => {hideElemById("find-game-panel"); showElemById("host-or-join-panel");},
     };
     for (const btnName in buttonActions) {
         document.getElementById(btnName).onclick = buttonActions[btnName];
@@ -40,7 +85,17 @@ window.addEventListener("load", function() {
 
     // === Set up other actions ===
     document.getElementById("username-field").addEventListener("input", function(e) {
-        const username = e.target.value;
+        const username = e.target.value.trim();
         document.getElementById("multi-player-btn").disabled = !isValidUsername(username);
+    });
+    document.getElementById("game-code-field").addEventListener("input", function(e) {
+        const gameCode = e.target.value.trim();
+        document.getElementById("join-game-btn").disabled = !(gameCode.length > 0);
+        // --- uncheck any games selected ---
+        const radioElems = document.getElementById("multi-games-widget").getElementsByClassName("game-select");
+        for (let i=0; i < radioElems.length; i++) {
+            const radioElem = radioElems[i];
+            radioElem.checked = false;
+        }
     });
 });
